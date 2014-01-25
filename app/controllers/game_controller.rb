8025ce16209a825703f64bfc33cfeb58d1142ecf
Simone D'Amico <sim@me.com>
2014-01-25 12:34:15 +0100

    implemented another way to initialiase scene as saw in the official doc

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,6 +6,7 @@ class GameController < UIViewController
     sk_view = self.view
     sk_view.showsFPS = true
     sk_view.showsNodeCount = true
+    sk_view.showsDrawCount = true
     scene = LevelOneScene.sceneWithSize(sk_view.bounds.size)
     scene.scaleMode = SKSceneScaleModeAspectFill
     sk_view.presentScene(scene)
--- app/scenes/level_one_scene.rb
+++ app/scenes/level_one_scene.rb
@@ -1,7 +1,20 @@
 class LevelOneScene < SKScene
-  def initWithSize(size)
-    super.tap do |scene|
-      scene.backgroundColor = UIColor.redColor
+  attr_accessor :content_created
+
+  # def initWithSize(size)
+  #   super.tap do |scene|
+  #     scene.backgroundColor = UIColor.redColor
+  #   end
+  # end
+
+  def didMoveToView(view)
+    unless content_created
+      create_scene_contents
+      self.content_created = true
     end
+  end
+
+  def create_scene_contents
+    self.backgroundColor = UIColor.greenColor
   end
 end
